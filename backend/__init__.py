2ab1cf80e0ccd52ee27b234bfdc4d3a95891b285
Scigi20 <sciommeri92@gmail.com>
2025-10-06 13:21:03 +0200

    Shoot me
    Backwards

---
+++ backend/__init__.py
@@ -0,0 +1,10 @@
+# -----------------------------
+# This file marks the 'backend' folder as a Python package.
+#
+# It is required so other modules (e.g. notification.py)
+# can import from files like get_function.py using:
+# from backend.get_function import ...
+#
+# No code is executed here.
+# -----------------------------
+
