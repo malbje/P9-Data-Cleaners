87764451dbfef5c035f0128a5262ea6b016e1413
Elia Ullerichs <euller21@student.aau.dk>
2025-11-27 09:26:14 +0100

    yea

--- LLM_main_class.py
+++ LLM_main_class.py
@@ -8,6 +8,8 @@
 
 what of this line
 
+and this line
+
 from openai import OpenAI
 import private_settings  # Contains OPENAI_API_KEY - referenced from private_settings.py
 from database.DB_read import DB_read
